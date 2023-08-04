b21dc6ee0ff60725106144eb1c8f02f09476e049
Jingasan <vc@Jinga>
2023-08-04 12:30:54 +0900

    <add> initial commit

--- 03_docker/express/src/index.ts
+++ 03_docker/express/src/index.ts
@@ -1,6 +1,5 @@
-import express, { Application, Request, Response, NextFunction } from "express";
+import express, { Application, Request, Response } from "express";
 import cors from "cors";
-import { GetRouter } from "./router";
 const app: Application = express();
 const PORT = 3000;
 // リクエストボディのパース用設定
@@ -8,17 +7,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // CORS設定
 app.use(cors());
-// Routerの追加
-app.use("/users", GetRouter());
 // GET
 app.get("/", async (req: Request, res: Response) => {
   return res.status(200).send({
     message: "Hello World!",
   });
-});
-// Error 404 Not Found
-app.use(async (_req: Request, res: Response, _next: NextFunction) => {
-  return res.status(404).json({ error: "Not Found" });
 });
 // サーバーを起動する処理
 try {
