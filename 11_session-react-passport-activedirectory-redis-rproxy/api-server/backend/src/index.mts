9b0c1af7c38702a0dc84fc7cab31a1a78882e0f8
jinga <xxx@gmail.com>
2024-01-19 18:42:36 +0900

    <add> initial commit

---
+++ 11_session-react-passport-activedirectory-redis-rproxy/api-server/backend/src/index.mts
@@ -0,0 +1,49 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import { authRouter } from "./auth.mjs";
+import { shoppingRouter } from "./shopping.mjs";
+const app = express();
+const PORT = Number(process.env.NODE_API_SERVER_PORT);
+// Secure Cookieを発行する場合に必要な設定
+app.set("trust proxy", 1);
+// リクエストボディのパース設定
+// 1. JSON形式のリクエストボディをパースできるように設定
+// 2. フォームの内容をパースできるように設定
+// 3. Payload Too Large エラー対策：50MBまでのデータをPOSTできるように設定(default:100kb)
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+// CORS設定
+app.use(cors());
+// Webページの提供
+app.use(express.static("/frontend"));
+
+/**
+ * 認証関連のAPI
+ */
+app.use("/api", authRouter());
+
+/**
+ * 買い物関連のAPI
+ */
+app.use("/api/shopping", shoppingRouter());
+
+/**
+ * Error 404 Not Found
+ */
+app.use((_req: Request, res: Response) => {
+  return res.status(404).json({ error: "Not Found" });
+});
+
+/**
+ * サーバーの起動処理
+ */
+try {
+  app.listen(PORT, () => {
+    console.log("server running at port:" + PORT);
+  });
+} catch (e) {
+  if (e instanceof Error) {
+    console.error(e.message);
+  }
+  process.exit(-1);
+}
---
+++ 11_session-react-passport-activedirectory-redis-rproxy/api-server/frontend/src/App.tsx
@@ -0,0 +1,87 @@
+import React from "react";
+import { checkAuthAPI, loginAPI, logoutAPI } from "./CallAPI";
+import Shopping from "./Shopping";
+
+/**
+ * ログインページ
+ */
+export default function App() {
+  // ログイン済みかどうか
+  const [isLogin, setIsLogin] = React.useState(false);
+  // 表示メッセージ
+  const [displayMessage, setDisplayMessage] = React.useState(<div></div>);
+  // ユーザー名／パスワード
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  /**
+   * 画面の初期化：ログイン状態をチェック
+   */
+  React.useEffect(() => {
+    setDisplayMessage(<div></div>);
+    const init = async () => setIsLogin(await checkAuthAPI());
+    init();
+  }, []);
+
+  /**
+   * ログイン処理
+   */
+  const handleLogin = async () => {
+    const res = await loginAPI(username, password);
+    // ステータスコード200の場合はログイン
+    if (res === 200) setIsLogin(true);
+    else setIsLogin(false);
+    // エラーメッセージの表示切替
+    if (res === 401 || res === false) {
+      setDisplayMessage(
+        <div style={{ color: "red" }}>エラー：ログイン認証に失敗しました。</div>
+      );
+    } else if (res === 429) {
+      setDisplayMessage(
+        <div style={{ color: "red" }}>
+          エラー：ログイン数が上限に達しました。
+        </div>
+      );
+    }
+  };
+
+  /**
+   * ログアウト処理
+   */
+  const handleLogout = async () => {
+    await logoutAPI();
+    setIsLogin(false);
+  };
+
+  return (
+    <div style={{ margin: "30px" }} className="App">
+      {isLogin ? (
+        <div>
+          <Shopping />
+          <br />
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+      ) : (
+        <div>
+          <h1>ログイン画面</h1>
+          <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button onClick={handleLogin}>Login</button>
+          <br />
+          <br />
+          {displayMessage}
+        </div>
+      )}
+    </div>
+  );
+}
